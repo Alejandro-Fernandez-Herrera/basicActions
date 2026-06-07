75edcdd21a3c8365340d6b1bb09c35b146806b3c
Alejandro Fernandez <alejandro.fernandez.herrera@correounivalle.edu.co>
2026-06-07 16:25:55 -0500

    fix: remove deprecated .eslintrc.js, using eslint.config.js

--- .eslintrc.js
+++
@@ -1,9 +0,0 @@
-module.exports = {
-  env: { node: true, es2021: true, jest: true },
-  extends: 'eslint:recommended',
-  rules: {
-    'complexity': ['warn', 5],
-    'no-unused-vars': 'warn',
-    'eqeqeq': ['error', 'always']
-  }
-};
