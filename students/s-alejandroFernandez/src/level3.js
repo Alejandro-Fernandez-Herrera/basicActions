f457b5ce0bd5fb7cc7ac410144f2248372301ebe
Alejandro Fernandez <alejandro.fernandez.herrera@correounivalle.edu.co>
2026-06-08 12:02:32 -0500

    feat: add password validator function with tests for level3

--- students/s-alejandroFernandez/src/level3.js
+++ students/s-alejandroFernandez/src/level3.js
@@ -16,4 +16,28 @@ function calculator(a, b, operator) {
   }
 }
 
-module.exports = { calculator };
+
+
+// --- Function 2 ---
+function validatePassword(password) {
+  if (typeof password !== 'string') throw new Error('Input must be a string');
+  const result = {
+    strong: false,
+    errors: []
+  };
+  if (password.length < 8)
+    result.errors.push('Must be at least 8 characters');
+  if (!/[A-Z]/.test(password))
+    result.errors.push('Must contain at least one uppercase letter');
+  if (!/[a-z]/.test(password))
+    result.errors.push('Must contain at least one lowercase letter');
+  if (!/[0-9]/.test(password))
+    result.errors.push('Must contain at least one number');
+  if (!/[^A-Za-z0-9]/.test(password))
+    result.errors.push('Must contain at least one special character');
+  result.strong = result.errors.length === 0;
+  return result;
+}
+
+
+module.exports = { calculator, validatePassword };
